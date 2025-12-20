ab0db168830f8fd5dcc9fbd79ec6877d53a5c678
qscheetz <qscheetz@pdx.edu>
2025-12-19 17:36:25 -0800

    now rendering 3d!

--- model.cpp
+++ model.cpp
@@ -59,7 +59,7 @@ Model::Vertex::getAttributeDescriptions() {
   std::vector<VkVertexInputAttributeDescription> attributeDescs(2);
   attributeDescs[0].binding = 0;
   attributeDescs[0].location = 0;
-  attributeDescs[0].format = VK_FORMAT_R32G32_SFLOAT;
+  attributeDescs[0].format = VK_FORMAT_R32G32B32_SFLOAT;
   attributeDescs[0].offset = offsetof(Vertex, position);
 
   attributeDescs[1].binding = 0;
